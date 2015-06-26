946eb703aa3d7d086dbe37028ecb587e856c7257
p <p@inam>
2015-06-27 00:29:37 +0700

    เพิ่ม/แก้   บลาๆๆๆๆ

--- views/site/index.php
+++ views/site/index.php
@@ -8,6 +8,9 @@ $this->title = 'Death Cause';
 
 //use miloschuman\highcharts\Highcharts;
 use dosamigos\chartjs\ChartJs;
+use miloschuman\highcharts\Highcharts;
+use scotthuangzl\googlechart\GoogleChart;
+use yii\helpers\Url;
 ?>
 
 <div class="site-index">
@@ -74,40 +77,11 @@ use dosamigos\chartjs\ChartJs;
                     <?php echo app\models\Year::find()->count(); ?>
                 </span>
             </div><!-- /.info-box-content -->
-        </div><!-- /.info-box -->
-       
+        </div><!-- /.info-box -->       
     </div><!-- /.col -->
 </div><!-- /.row -->
 
-<?php // echo ChartJs::widget([
-//    'type' => 'Line',
-//    'options' => [
-//        'height' => 400,
-//        'width' => 800
-//    ],
-//    'data' => [
-//        'labels' => ["2006","2007", "2008", "2009","2010", "2011", "2012", "2013", "2014"],
-//        'datasets' => [
-//            [
-//                'fillColor' => "rgba(220,220,220,0.5)",
-//                'strokeColor' => "rgba(220,220,220,1)",
-//                'pointColor' => "rgba(220,220,220,1)",
-//                'pointStrokeColor' => "#fff",
-//                'series'=>'เพศชาย',
-//                'data' => [823, 3220, 3218, 3229, 3300, 3302, 3555,3586,2633]
-//            ],
-//            [
-//                'fillColor' => "rgba(151,187,205,0.5)",
-//                'strokeColor' => "rgba(151,187,205,1)",
-//                'pointColor' => "rgba(151,187,205,1)",
-//                'pointStrokeColor' => "#fff",
-//                'data' => [639, 2441, 2460, 2467, 2644, 2596, 2746,2730,2050]
-//            ],
-//           
-//        ]
-//    ]
-//]);
-?>
+
 
 <?=
 \dosamigos\highcharts\HighCharts::widget([
@@ -145,11 +119,154 @@ use dosamigos\chartjs\ChartJs;
     ]
 ]);?>
 
-    <div class="jumbotron">
-        <h3>สาเหตุการตาย</h3>        
     </div>
-    <div class="body-content">
 
+<div class="row">
+    <!-- Left col -->
+    <div class="col-md-8">
+        <!-- MAP & BOX PANE -->
+        <div class="box box-success">
+            <div class="box-header with-border">
+                <h3 class="box-title">การตายแยกตามกลุ่มโรค(5อันดับแรก)</h3>
+                <div class="box-tools pull-right">
+                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    
+                </div>
+            </div><!-- /.box-header -->
+            <div class="box-body no-padding">
+                <div class="row">
+                    <div class="col-md-9 col-sm-8">
+                        <div class="pad">
+                            <!-- Map will be created here -->
+                            <div id="world-map-markers" style="height: 200px;">
+                                
+                                <?php 
+                        echo GoogleChart::widget(array('visualization' => 'PieChart',
+                            'data' => array(
+                                array('Task', 'Hours per Day'),
+                                array('R99', 10707),
+                                array('A419', 5089),
+                                array('R54', 3788),
+                                array('J189', 1786),
+                                array('Y349', 1590)
+                                    ),
+                                    'options' => array('title' => '')));
+                                ?>
+                            </div>
+                        </div>
+                    </div><!-- /.col -->
+                    
+                </div><!-- /.row -->
+            </div><!-- /.box-body -->
+        </div><!-- /.box -->
+    
+
+        <!-- TABLE: LATEST ORDERS -->
+        <div class="box box-info">
+            <div class="box-header with-border">
+                <h3 class="box-title">สาเหตุการตาย</h3>
+                <div class="box-tools pull-right">
+                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                </div>
+            </div><!-- /.box-header -->
+            <div class="box-body">
+                <div class="table-responsive">
+                    <table class="table no-margin">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th></th>
+                                <th></th>                                
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><a href="<?php echo Url::to(['occupation/index']); ?>">สาเหตุการตายแยกตามกลุ่มอาชีพ</a></td>
+                                <td>สาเหตุการตายแยกตามกลุ่มอาชีพ</td>
+                                <td><span class="label label-success">Occupation</span></td>                                
+                            </tr>
+                            <tr>
+                                <td><a href="<?php echo Url::to(['disease/index']); ?>">สาเหตุการตายแยกตามกลุ่มโรค</a></td>
+                                <td>สาเหตุการตายแยกตามกลุ่มโรค</td>
+                                <td><span class="label label-warning">Disease</span></td> 
+                                
+                            </tr>
+                            <tr>
+                                <td><a href="<?php echo Url::to(['sex/index']); ?>">สาเหตุการตายแยกตามเพศ</a></td>
+                                <td>สาเหตุการตายแยกตามเพศ</td>
+                                <td><span class="label label-danger">Sex</span></td>
+                                
+                            </tr>
+                            <tr>
+                                <td><a href="<?php echo Url::to(['year/index']); ?>">สาเหตุการตายแยกรายปี</a></td>
+                                <td>สาเหตุการตายแยกรายปี</td>
+                                <td><span class="label label-info">Year</span></td>
+                                
+                            </tr>
+                            
+                        </tbody>
+                    </table>
+                </div><!-- /.table-responsive -->
+            </div><!-- /.box-body -->
+            <div class="box-footer clearfix">
+                <a href="javascript::;" class="btn btn-sm btn-info btn-flat pull-left">Place New Order</a>
+                <a href="javascript::;" class="btn btn-sm btn-default btn-flat pull-right">View All Orders</a>
+            </div><!-- /.box-footer -->
+        </div><!-- /.box -->
+    </div><!-- /.col -->
+
+    <div class="col-md-4">
+        <!-- Info Boxes Style 2 -->
+        <div class="info-box bg-yellow">
+            <span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Disease</span>
+                <span class="info-box-number">21,348</span>
+                <div class="progress">
+                    <div class="progress-bar" style="width: 50%"></div>
+                </div>
+                
+            </div><!-- /.info-box-content -->
+        </div><!-- /.info-box -->
+        <div class="info-box bg-green">
+            <span class="info-box-icon"><i class="ion ion-ios-heart-outline"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Occupation</span>
+                <span class="info-box-number">31,680</span>
+                <div class="progress">
+                    <div class="progress-bar" style="width: 20%"></div>
+                </div>
+                
+            </div><!-- /.info-box-content -->
+        </div><!-- /.info-box -->
+        <div class="info-box bg-red">
+            <span class="info-box-icon"><i class="ion ion-ios-cloud-download-outline"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Sex</span>
+                <span class="info-box-number">1,675</span>
+                <div class="progress">
+                    <div class="progress-bar" style="width: 70%"></div>
+                </div>
+                
+            </div><!-- /.info-box-content -->
+        </div><!-- /.info-box -->
+        <div class="info-box bg-aqua">
+            <span class="info-box-icon"><i class="ion-ios-chatbubble-outline"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Year</span>
+                <span class="info-box-number">742</span>
+                <div class="progress">
+                    <div class="progress-bar" style="width: 40%"></div>
+                </div>
+                
+            </div><!-- /.info-box-content -->
+        </div><!-- /.info-box -->
+       
+    </div><!-- /.col -->
+</div>    
+    <div class="body-content">
+        
     </div>
 </div>
 <?php
